170553f2f6ae6c17b29f3b49bfe6fd02dc637db5
Ayodele Olufemi Segun <feminiscence@gmail.com>
2017-08-11 19:39:17 +0100

    uploaded files to api folder

---
+++ api/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+/* FUNCTIONS FOR FILTERING JSON */
+
+	function after ($this, $inthat)
+    {
+        if (!is_bool(strpos($inthat, $this)))
+        return substr($inthat, strpos($inthat,$this)+strlen($this));
+    };
+
+    function before ($this, $inthat)
+    {
+        return substr($inthat, 0, strpos($inthat, $this));
+    };
+
+    function between ($this, $that, $inthat)
+    {
+        return before ($that, after($this, $inthat));
+    };
+	
+/* JSON FUNCTION */
+
+	function getjson( $appid) {
+		if(!file_get_contents('https://itunes.apple.com/rss/customerreviews/id=' . $appid . '/json')) {
+			
+			echo "<script>alert('Unable to get json_data')</script>";
+			die("");
+		} else {
+			
+			$json_data = file_get_contents('https://itunes.apple.com/rss/customerreviews/id=' . $appid . '/json');
+			return $json_data;
+		}
+	}
+
+	?>
